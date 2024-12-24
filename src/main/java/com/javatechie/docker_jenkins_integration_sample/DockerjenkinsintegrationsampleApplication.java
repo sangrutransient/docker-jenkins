3bda83bc0e8efc8d2a9047120ef946dc7d8b58fb
sangrutransient <sangru.transient@gmail.com>
2024-12-24 20:51:18 +0530

    first commit

---
+++ src/main/java/com/javatechie/docker_jenkins_integration_sample/DockerjenkinsintegrationsampleApplication.java
@@ -0,0 +1,13 @@
+package com.javatechie.docker_jenkins_integration_sample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DockerjenkinsintegrationsampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DockerjenkinsintegrationsampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/javatechie/docker_jenkins_integration_sample/DockerjenkinsintegrationsampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javatechie.docker_jenkins_integration_sample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DockerjenkinsintegrationsampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
