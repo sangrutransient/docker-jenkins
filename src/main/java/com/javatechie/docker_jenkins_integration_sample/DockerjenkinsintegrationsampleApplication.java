952b2f7398d40c33f970c795f4bfac239d24e231
sangrutransient <sangru.transient@gmail.com>
2024-12-27 00:13:42 +0530

    Second Commit

--- src/main/java/com/javatechie/docker_jenkins_integration_sample/DockerjenkinsintegrationsampleApplication.java
+++ src/main/java/com/javatechie/docker_jenkins_integration_sample/DockerjenkinsintegrationsampleApplication.java
@@ -1,5 +1,6 @@
 package com.javatechie.docker_jenkins_integration_sample;
 
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
